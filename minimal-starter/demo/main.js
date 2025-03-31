dd09d6dccba84eda528b83d34f6522d1565236d1
Sergiu_Bo <bocanceaserge335566@gmail.com>
2025-03-31 13:44:29 +0200

    :bento: Add monkey model

--- minimal-starter/demo/main.js
+++ minimal-starter/demo/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { Pane } from 'tweakpane';
 
 const textureLoader = new THREE.TextureLoader();
 
@@ -10,7 +11,6 @@ let camera = null
 
 let mesh = null
 
-let albedoTexture = null;
 
 function init() {
     createRenderer()
@@ -18,6 +18,7 @@ function init() {
     createCamera()
     createLights()
     createLoader()
+    
     animate()
 }
 
@@ -29,29 +30,50 @@ function createRenderer() {
     renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-function createLoader() {
-    textureLoader.load('albedo.png', (texture) => {
-        albedoTexture = texture;
-        createMesh();
-    })
+let albedoTexture = null;
+let normalTexture = null;
+let aoTexture = null;
+let roughnessTexture = null;
+let heightTexture = null;
+
+async function createLoader() {
+    albedoTexture = await textureLoader.load('albedo.png')
+    normalTexture = await textureLoader.load('normal.png')
+    aoTexture = await textureLoader.load('ao.png')
+    roughnessTexture = await textureLoader.load('roughness.png')
+    heightTexture = await textureLoader.load('height.png')
+    createMesh();
+
+    createDebugger()
 }
+
 function createCamera() {
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.z = 5
+    camera.position.z = 15
+    camera.position.y = 14
+
+    camera.lookAt(0, 0, 0)
 }
 function createScene() {
     scene = new THREE.Scene()
 }
 
 function createMesh() {
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const geometry = new THREE.PlaneGeometry(1, 1, 128, 128)
     const material = new THREE.MeshStandardMaterial({ 
-        color: new THREE.Color('#ff0000'), 
         map: albedoTexture,
+        normalMap: normalTexture,
+        normalScale: new THREE.Vector2(10, 10),
+        aoMap: aoTexture,
+        aoMapIntensity : 1,
+        roughnessMap: roughnessTexture,
+        displacementMap: heightTexture,
+        displacementScale: 0.1,
     })
     
     mesh = new THREE.Mesh(geometry, material)
-    
+    mesh.rotation.x = -Math.PI / 2
+    mesh.scale.set(20, 20, 20)
     scene.add(mesh)
 }
 
@@ -64,13 +86,15 @@ function createLights() {
     scene.add(directionalLight)
 }
 
+function createDebugger() {
+    const pane = new Pane()
+    const folder = pane.addFolder({ title: 'Mesh', expanded: true })
+    folder.addBinding(mesh, 'rotation', { label: 'Rotation'})
+    folder.addBinding(mesh.material, 'aoMapIntensity', { label: 'AO Map Intensity' })
+}
 function animate() {
     requestAnimationFrame(animate)
 
-    if(mesh) {
-        mesh.rotation.x += 0.01
-        mesh.rotation.y += 0.01
-    }
     renderer.render(scene, camera)
 }
 
