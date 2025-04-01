2c299ca9a62142c6be4cc3d90ecc743799ca904d
Rémy Bonté-Duval <remy.bonteduval@prosehair.com>
2025-04-01 10:13:13 +0200

    add st variable (aka 'normalized' vUv)

--- minimal-starter/demo/fragment.glsl
+++ minimal-starter/demo/fragment.glsl
@@ -16,17 +16,19 @@ float circle(vec2 st, vec2 origin, float size) {
 void main() {
     float progress[circlesCount];
 
+    vec2 st = vec2(vUv.x * aspectRatio, vUv.y);
+
     // vec3 finalColor = vec3(.0, .0, .0); // basis for adding colors ?
     // for(int i = 0; i < circlesCount; i++) {
     //     progress[i] = fract(u_time * 0.1 + cos(float(i)));
-    //     vec3 circleCol = circle(vUv, circles[i], progress[i]) * proseColors[i % colorsCount] * (1.0 - pow(progress[i], .3));
+    //     vec3 circleCol = circle(st, circles[i], progress[i]) * proseColors[i % colorsCount] * (1.0 - pow(progress[i], .3));
     //     finalColor += circleCol;
     // }
 
     vec3 finalColor = vec3(.5); // basis for multiplying colors ?
     for(int i = 0; i < circlesCount; i++) {
         progress[i] = fract(u_time * 0.1 + cos(float(i)));
-        float circle = circle(vUv, circles[i], progress[i]) * (1.0 - pow(progress[i], 2.0));
+        float circle = circle(st, circles[i], progress[i]) * (1.0 - pow(progress[i], 2.0));
         finalColor = mix(finalColor, proseColors[i % colorsCount], circle);
     }
 
